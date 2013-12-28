7d65e4ac727c2052c1d59dd80ee0b56b76feb677
shuttler67 <shuttler67@gmail.com>
2013-12-28 13:57:03 +0100

    -Moved projectile handling to Player class from Level class -Added unused and unfinished Button class -Changed mouse input handling 	-firstMousedown and firstMousedown booleans work now -Added ability to disable the player in a specific level 	*in level initialization change first arguement to NIL -Works

--- PixelToy 1.4/script.py
+++ PixelToy 1.4/script.py
@@ -12,10 +12,14 @@ RIGHT = "right"
 LEFT  = "left"
 UP    = "up"
 DOWN  = "down"
+NIL   = "nil"
 
 MAXSPEED = 2.7
 
+allKeysUsed = ('w','a','s','d')
 
+RED = "red"
+GREEN = "green"
 class Rect:
 	def __init__(self,x,y,w,h):
 		self.x1 = x
@@ -44,6 +48,26 @@ class Rect:
 			return True
 		return False
 
+class Button:
+	def __init__(self,rect,text,function):
+		self.rect = rect
+		self.text = text
+		self.function = function
+		self.colour = RED
+	def checkClick(self,mousex,mousey,mousedown,firstMouseup):
+		if self.rect.rectCollide(Rect(mousex,mousey,0,0)): #checking if mouse is over button
+			if mousedown:
+				self.colour = GREEN
+			else:
+				self.colour = RED
+			if firstMouseup:
+				function()
+	def draw(self):
+		if self.colour == RED:
+			useColour(255,0,0,255)
+		elif self.colour == GREEN:
+			drawRectangle(self.rect.x1,self.rect.y1,self.rect.width,self.rect.height)
+		
 class Wall:
 	def __init__(self,rect):
 		self.rect = rect
@@ -105,17 +129,30 @@ class Player:
 		self.speedx = 0
 		self.speedy = 0
 		self.inAttack = False
+		self.projSize = 3
+		self.proj = []
+	def powerup(self,mousex,mousey,firstMousedown):
+		if firstMousedown:
+			self.proj.append(Projectile())
+		if self.projSize <= 6:
+			self.projSize += 0.1
+		self.proj[-1].update(self.projSize,False,self.x,self.y+self.size/2+self.projSize/2,mousex,mousey)
+		self.inAttack = True
+	def attack(self,mousex,mousey):
+		self.proj[-1].update(self.projSize,True,self.x,self.y+self.size/2+self.projSize/2,mousex,mousey)
+		self.projSize = 3
+		self.inAttack = False
+	def move(self,canNotMoves,pressedKeys):
 		self.directions = []
-	def move(self,canNotMoves):
-		self.directions = []
-		if isKeyDown('w'):
-			self.speedy += 0.3
-		if isKeyDown('s'):
-			self.speedy -= 0.3
-		if isKeyDown('a'):
-			self.speedx -= 0.3
-		if isKeyDown('d'):
-			self.speedx += 0.3
+		for key in pressedKeys:
+			if key == 'w':
+				self.speedy += 0.3
+			if key == 'a':
+				self.speedx -= 0.3
+			if key == 's':
+				self.speedy -= 0.3
+			if key == 'd':
+				self.speedx += 0.3
 		self.speedx *= 0.9
 		self.speedy *= 0.9
 		if self.speedx > 0:
@@ -126,8 +163,6 @@ class Player:
 			self.directions.append(UP)
 		if self.speedy < 0:
 			self.directions.append(DOWN)
-		print canNotMoves
-		#print directions
 
 		for direction in self.directions:
 			for canNotMove in canNotMoves:
@@ -139,12 +174,13 @@ class Player:
 					
 		self.x += self.speedx
 		self.y += self.speedy
-	def powerup(self,projSize,mousex,mousey,proj):
-		proj[-1].update(projSize,False,self.x,self.y+self.size/2+projSize/2,mousex,mousey)
-		self.inAttack = True
-	def attack(self,projSize,mousex,mousey,proj):
-		proj[-1].update(projSize,True,self.x,self.y+self.size/2+projSize/2,mousex,mousey)
-		self.inAttack = False
+
+		for j in range(len(self.proj), 0, -1):
+			i = j-1
+			self.proj[i].move()
+			self.proj[i].draw()
+			if self.proj[i].checkIfOut():
+				del self.proj[i]
 	def draw(self):
 		if self.inAttack:
 			drawImage(man2,self.x,self.y,self.size,self.size)
@@ -154,46 +190,47 @@ class Player:
 	
 		
 class Level:
-	def __init__(self,mx,my):
-		self.man = Player(mx,my)
-		self.firstMousedown = True
+	def __init__(self,mx,my,buttons):
+		self.hasMan = False
+		if not mx == NIL:
+			self.man = Player(mx,my)
+			self.hasMan = True
+		self.buttons = buttons
 		self.mousedown = False
-		self.projSize = 3
-		self.proj = []
+		self.pressedkeys = []
 	def updateObstacles(self,walls,enemies):
 		self.walls = walls
 		self.enemies = enemies
 	def mainLoop(self):
 		canNotMoves = []
-		if isLeftMouseDown():
+		self.pressedKeys = []
+		firstMousedown = False
+		firstMouseup = False
+		if isLeftMouseDown() and self.mousedown == False:
+			firstMousedown = True
 			self.mousedown = True
-			if self.firstMousedown:
-				self.proj.append(Projectile())
-				self.firstMousedown = False
-			if self.projSize <= 6:
-				self.projSize += 0.1
-			self.man.powerup(self.projSize,_mouseX,_mouseY,self.proj)
 		if not isLeftMouseDown() and self.mousedown == True:
-			self.man.attack(self.projSize,_mouseX,_mouseY,self.proj)
-			self.projSize = 3
+			firstMouseup = True
 			self.mousedown = False
-			self.firstMousedown = True
 		
+		for key in allKeysUsed:
+			if isKeyDown(key):
+				self.pressedKeys.append(key)
+			
 		for wall in self.walls:
 			wall.draw()
-			canNotMoves += wall.playerCollide(self.man.x,self.man.y,self.man.size)
+			if self.hasMan:
+				canNotMoves += wall.playerCollide(self.man.x,self.man.y,self.man.size)
 
-		for j in range(len(self.proj), 0, -1):
-			i = j-1
-			self.proj[i].move()
-			self.proj[i].draw()
-			if self.proj[i].checkIfOut():
-				del self.proj[i]
-		
-		self.man.move(canNotMoves)
-		self.man.draw()
+		if self.hasMan:
+			if self.mousedown:
+				self.man.powerup(_mouseX,_mouseY,firstMousedown)
+			if firstMouseup:
+				self.man.attack(_mouseX,_mouseY)
+			self.man.move(canNotMoves,self.pressedKeys)
+			self.man.draw() 
 
-LVL1= {"LVL":Level(32,_screenHeight/2),"WALLS":[Wall(Rect(100,100,100,10)),Wall(Rect(200,100,10,100))],"ENEMIES":[0,1]}
+LVL1= {"LVL":Level(50,_screenHeight/2,[1,2]),"WALLS":[Wall(Rect(100,100,100,10)),Wall(Rect(200,100,10,100))],"ENEMIES":[0,1]}
 currentLVL = LVL1
 currentLVL["LVL"].updateObstacles(currentLVL["WALLS"],currentLVL["ENEMIES"])
 while True:
