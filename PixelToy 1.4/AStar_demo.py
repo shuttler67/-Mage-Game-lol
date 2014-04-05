3d72f56acc26fa55af5375f7212ed644b95b9bff
shuttler67 <shuttler67@gmail.com>
2014-04-05 16:17:41 +0200

    BIG changes
    - put game code back into script.py
    - saved astar as AStar_demo.py
    - changed astar.py to fit game needs
    - managed to FIX importing,
    	-files to be imported need to be inside PixelTo.jar
    	-put utilities and astar in PixelToy.jar
    	-changed button class to require mousePos input
    -deleted scriptORIG.py

--- PixelToy 1.4/AStar_demo.py
+++ PixelToy 1.4/AStar_demo.py
@@ -1,14 +1,12 @@
-
+#!/usr/home/thomas/git/-Mage-Game-lol/PixelToy\ 1.4
 
 class AStar:
-	def __init__():
-		pass
 	@staticmethod
 	def estimateHeuristicTo(goal, node):
 		deltaX = abs(goal[0] - node.worldx)
 		deltaY = abs(goal[1] - node.worldy)
-		#return deltaX + deltaY
-		return min(deltaX, deltaY) * 1.4 + max(deltaX, deltaY) - min(deltaX, deltaY)
+		return deltaX + deltaY
+		#return min(deltaX, deltaY) * 1.4 + max(deltaX, deltaY) - min(deltaX, deltaY)
 		
 	class Node:
 		def __init__(self, worldx, worldy):
@@ -32,10 +30,17 @@ class AStar:
 					if not worldx == worldy == 0:
 						neighbours.append(AStar.Node(self.worldx+worldx,self.worldy+worldy))
 						neighbours[-1].calculateScores(self.g_score+distance, goal, (self.worldx,self.worldy))
-	    		return neighbours		
+	    		return neighbours
+	
+	def __init__(self, start, goal):
+		self.start = AStar.Node(*start)
+		self.goal = AStar.Node(*goal)
+		self.start.calculateScores(0, goal, None)
+		self.openNodes = [self.start]
+		self.closedNodes = []
 		
-	def recoverPath(self, goal = self.goal):
-		nodePath = [goal]
+	def recoverPath(self):
+		nodePath = [self.goal]
 		path = []
 		
 		while not nodePath[0] == self.start:
@@ -47,19 +52,28 @@ class AStar:
 		
 		return path
 
-	def findPath(self, start, goal, canMoveTo):
-		self.start = AStar.Node(*start)
-		self.goal = AStar.Node(*goal)
-		self.start.calculateScores(0, goal, None)
-		self.openNodes = [self.start]
-		self.closedNodes = []
-
+	def findPath(self, canMoveTo):
 		if not canMoveTo(self.goal):
 			return
 		foundGoal = False
 		fail = False
 		iteration = 0
 		while not foundGoal and not fail:
+			iteration += 0
+			if iteration % 5 == 0:
+				newFrame()
+			
+			for c_node in self.closedNodes:
+				useColour(0, 0, 255, 255)
+				drawCircle(c_node.worldx*15+100, c_node.worldy*15+100,5)
+			for o_node in self.openNodes:
+				useColour(255,0,0,255)
+				drawCircle(o_node.worldx*15+100, o_node.worldy*15+100,5)
+			useColour(0,255,0,255)
+			drawCircle(self.goal.worldx*15+100, self.goal.worldy*15+100, 5)
+
+			iteration += 1
+			#print iteration
 			if len(self.openNodes) != 0:
 				bestIndex = 0
 				min_f_score = 100000000
@@ -72,13 +86,14 @@ class AStar:
 				self.closedNodes.append(self.openNodes[bestIndex])
 				currentNode = self.openNodes[bestIndex]
 				del self.openNodes[bestIndex]
-
+				#print "F", currentNode.f_score
+				#print "G", currentNode.g_score
 				if currentNode == self.goal:
 					foundGoal = True
 					self.goal = currentNode
 					continue
 				currentNeighbours = currentNode.neighbourNodes((self.goal.worldx, self.goal.worldy))
-
+				#print "Before", len(currentNeighbours)
 				for i in range(len(currentNeighbours)-1,-1,-1):
 					if currentNeighbours[i] in self.closedNodes:
 						del currentNeighbours[i]
@@ -92,13 +107,37 @@ class AStar:
 							self.openNodes[open_index].calculateScores(currentNeighbours[i].g_score,(self.goal.worldx, self.goal.worldy),currentNeighbours[i].cameFromPos)
 						del currentNeighbours[i]
 							
+					
+				#print "After", len(currentNeighbours)
 				self.openNodes += currentNeighbours
+				#print len(self.closedNodes)
+				#print len(self.openNodes)
 
 			else:
 				fail = True
 
 		if not fail:
 			return self.recoverPath()
-		else:
-			return self.recoverPath(self.closedNodes[-1])
 
+def canMoveTo(node):
+	return node.worldx < -13 or node.worldx > 20 or node.worldy != 8
+ 
+a = AStar((0,0),(10,40))
+path = a.findPath(canMoveTo)
+while True:
+	newFrame()
+	useColour(0, 0, 0, 255)
+	drawString(0 , 0, str((_mouseX-100) / 15) + ',' + str((_mouseY-100) / 15))
+	for c_node in a.closedNodes:
+		useColour(0, 0, 255, 255)
+		drawCircle(c_node.worldx*15+100, c_node.worldy*15+100,5)
+	for o_node in a.openNodes:
+		useColour(255,0,0,255)
+		drawCircle(o_node.worldx*15+100, o_node.worldy*15+100,5)
+	if path != None:
+		for p in path:
+			useColour(0,255,0,255)
+			drawCircle(p[0]*15+100, p[1]*15+100,5)
+	else:
+		useColour(0, 0, 0, 255)
+		drawString(_screenWidth, _screenHeight, "NO SOLUTION")
